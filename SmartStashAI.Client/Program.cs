df267df2e4c9f9c09a3804013217c9f5034edd0c
Siewier <przemeksiewierski@gmail.com>
2026-06-06 00:42:52 +0200

    Dodany appsettings do kienta

--- SmartStashAI.Client/Program.cs
+++ SmartStashAI.Client/Program.cs
@@ -4,17 +4,22 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartStashAI.Client;
 using SmartStashAI.Client.Auth;
 
-// 1. Inicjalizacja buildera z poprawnym przekazaniem tablicy args
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// 2. Konfiguracja HttpClient – celujemy w Twój lokalny backend API
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7047/") });
+// Czytamy adres API z konfiguracji zamiast wpisywaæ na sztywno
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"]
+    ?? "https://localhost:7047/";
 
-// 3. Rejestracja us³ug zarz¹dzania stanem autoryzacji i tokenów JWT
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(apiBaseUrl)
+});
+
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<JwtAuthenticationStateProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<JwtAuthenticationStateProvider>());
+builder.Services.AddScoped<AuthenticationStateProvider>(
+    sp => sp.GetRequiredService<JwtAuthenticationStateProvider>());
 
 await builder.Build().RunAsync();
